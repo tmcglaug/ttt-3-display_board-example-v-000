6c277a8591f3e84243cb72003c18e53682f09ca7
Thomas McGlaughlin <tmm86507@gmail.com>
2017-10-03 15:08:39 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,17 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-print
-"   |   |
------------
-   |   |
------------
-   |   |   "
+  cell = "   "
+  cellSeparator = "|"
+  line separator = "-----------"
+  line = cell + cellSeparator +  cell + cellSeparator + cell
+
+  puts line
+  puts lineSeparator
+  puts line
+  puts lineSeparator
+  puts line
+  
+
 
 end
 
