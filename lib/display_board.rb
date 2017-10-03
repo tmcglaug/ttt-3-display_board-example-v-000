be2402307d0cf9d2da37d0c7d3ad79dfd6a7c8f8
Thomas McGlaughlin <tmm86507@gmail.com>
2017-10-03 15:04:52 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,14 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-prints "```
-   |   |   
+print "```
+   |   |
 -----------
-   |   |   
+   |   |
 -----------
-   |   |   
+   |   |
 ```"
 
 end
+
+display_board
+
